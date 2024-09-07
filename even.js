10064ef73163261c25325617872d247fcca8ac9f
ashraful-islam-siam <ashrafullislam.dev1@gmail.com>
2024-09-07 12:05:47 +0600

    added solution of isEven and isOdd problem

---
+++ even.js
@@ -0,0 +1,11 @@
+function isEven(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(isEven(5));
+console.log(isEven(58));
+console.log(isEven(7));
---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(n) {
+  if (n % 2 === 1) {
+    console.log("Odd");
+  } else {
+    console.log("Even");
+  }
+}
+
+isOdd(5);
+isOdd(8);
